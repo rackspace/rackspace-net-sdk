48dcbff784b6e0daefa34a1f24275c0c5b0c53cc
Carolyn Van Slyck <me@carolynvanslyck.com>
2015-08-13 18:17:21 -0500

    Add synchronous extension methods for IPage<T>

--- src/Rackspace/IPage.cs
+++ src/Rackspace/IPage.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using OpenStack.Synchronous.Extensions;
 
 namespace Rackspace
 {
@@ -19,5 +20,22 @@ namespace Rackspace
         /// Retrieves the next page of items, if one is available. If <see cref="HasNextPage"/> is <c>false</c>, an empty page is returned.
         /// </summary>
         Task<IPage<T>> GetNextPageAsync(CancellationToken cancellation = default(CancellationToken));
+    }
+}
+
+namespace Rackspace.Synchronous
+{
+    /// <summary>
+    /// Provides synchronous extention methods for an <see cref="IPage{T}"/> instance.
+    /// </summary>
+    public static class PageExtensions
+    {
+        /// <summary>
+        /// Retrieves the next page of items, if one is available. If <see cref="IPage{T}.HasNextPage"/> is <c>false</c>, an empty page is returned.
+        /// </summary>
+        public static IPage<T> GetNextPage<T>(this IPage<T> page)
+        {
+            return page.GetNextPageAsync().ForceSynchronous();
+        }
     }
 }
