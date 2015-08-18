0447ff13cc8ece9ec6e21cc28cc09fba4806d910
Carolyn Van Slyck <me@carolynvanslyck.com>
2015-08-18 10:41:43 -0500

    Add script to deploy generated sdk docs

---
+++ build/deploy-sdk-docs.sh
@@ -0,0 +1,13 @@
+# Apply latest docs
+rm -fr gh-pages/docs
+mv generated-docs gh-pages/docs
+
+# Prep gh-pages branch
+cd gh-pages
+git config user.name "Build Bot"
+git config user.email "build@openstacknetsdk.org"
+git add -A docs
+git commit -m "Deploy SDK docs @ $bamboo_repository_revision_number"
+
+# Push
+git push --force --quiet "https://$bamboo_deploy_password@github.com/rackspace/rackspace-net-sdk.git" gh-pages:gh-pages > /dev/null 2>&1
